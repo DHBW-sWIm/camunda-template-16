a7c718cc48b6cc1e72566664459fcef361e9beab
henrystoll <info.henry.stoll@gmail.com>
2019-06-20 15:49:30 +0200

    initial commit

---
+++ src/main/java/moodle/MailMgmtStudentIsIll.java
@@ -0,0 +1,63 @@
+package moodle;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.logging.Logger;
+
+public class MailMgmtStudentIsIll implements JavaDelegate {
+
+    private final Logger LOGGER = Logger.getLogger(LoggerDelegate.class.getName());
+
+    public void execute(DelegateExecution execution) throws IOException {
+
+        String studentName = (String) execution.getVariable("student_name");
+        String studentMatnr = (String) execution.getVariable("student_matnr");
+        String studentReason = (String) execution.getVariable("student_reason");
+        Date studentLength = (Date) execution.getVariable("student_length");
+
+
+
+//        LocalDateTime studentSickUntilDate = LocalDateTime.parse(studentLength);
+
+//        DateTimeFormatter  formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
+//        String formattedDate = formatter.format(studentSickUntilDate);
+
+
+
+        String content = "<h1>Sie haben eine neue Krankschreibung erhalten</h1>"
+                + "<p>Student: " + studentName + "</p>"
+                + "<p>Matrikelnummer: " + studentMatnr + "</p>"
+                + "<p>Grund der Krankschreibung: " + studentReason + "</p>"
+                + "<p>Bis: " + studentLength + "</p>"
+                + "<a href='www.moodle.com'>Link zum Moodle Bestätigungsform</a>";
+
+
+        String receiver = "test@test.com";
+        String subject = "Test Mail";
+        try {
+            Mail.send(receiver, subject, content);
+        } catch (MessagingException e) {
+            //TODO: refactor in own class
+            LOGGER.info("\n\n  ... LoggerDelegate invoked by "
+                    + "processDefinitionId=" + execution.getProcessDefinitionId()
+                    + ", activtyId=" + execution.getCurrentActivityId()
+                    + ", activtyName='" + execution.getCurrentActivityName() + "'"
+                    + ", processInstanceId=" + execution.getProcessInstanceId()
+                    + ", businessKey=" + execution.getProcessBusinessKey()
+                    + ", executionId=" + execution.getId()
+                    + " \n\n"
+                    + "STACKTRACE\n"
+                    + Arrays.toString(e.getStackTrace()));
+
+        }
+    }
+}
