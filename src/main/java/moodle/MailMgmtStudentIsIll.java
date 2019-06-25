8362fe38cdc152dbd7c25f84b646edeec6e69af5
henrystoll <info.henry.stoll@gmail.com>
2019-06-25 18:46:45 +0200

    comments and using Date.util again

--- src/main/java/moodle/MailMgmtStudentIsIll.java
+++ src/main/java/moodle/MailMgmtStudentIsIll.java
@@ -5,50 +5,46 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.logging.Logger;
 
 public class MailMgmtStudentIsIll implements JavaDelegate {
 
-    private final Logger LOGGER = Logger.getLogger(LoggerDelegate.class.getName());
+//    private final Logger LOGGER = Logger.getLogger(LoggerDelegate.class.getName());
 
     public void execute(DelegateExecution execution) throws IOException {
 
+        // [EXAMPLE] Get Camunda variables and work with them
         String studentName = (String) execution.getVariable("student_name");
         String studentMatnr = (String) execution.getVariable("student_matnr");
         String studentReason = (String) execution.getVariable("student_reason");
-        Date studentLength = (Date) execution.getVariable("student_length");
+        Date sickUntilDate = (Date) execution.getVariable("student_length");
 
-        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        String sickUntil = dateFormat.format(studentLength);
+        // formatting and converting the date - plase use LocalDateTime for Date or Time related stuff
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDateTime localSickUntil = new Timestamp(sickUntilDate.getTime()).toLocalDateTime();
+        String sickUntil = localSickUntil.format(formatter);
 
         String content = "<h1>Sie haben eine neue Krankschreibung erhalten</h1>"
                 + "<p>Student: " + studentName + "</p>"
                 + "<p>Matrikelnummer: " + studentMatnr + "</p>"
                 + "<p>Grund der Krankschreibung: " + studentReason + "</p>"
-                + "<p>Bis: " + sickUntil + "</p>"
-                + "<a href='www.moodle.com'>Link zum Moodle Bestätigungsform</a>";
+                + "<p>Bis: " + sickUntil  + "</p>"
+                + "<a href='https://www.moodle.com'>Link (not working) zum Moodle Bestätigungsform</a>";
 
 
+        // [EXAMPLE] setting a test variable
+        execution.setVariable("testvar", true);
+
+        // [EXAMPLE] Send Email with simple HTML Content
         String receiver = "test@test.com";
         String subject = "Test Mail";
         try {
             Mail.send(receiver, subject, content);
         } catch (MessagingException e) {
-//            TODO: refactor in own class
-            LOGGER.info("\n\n  ... LoggerDelegate invoked by "
-                    + "processDefinitionId=" + execution.getProcessDefinitionId()
-                    + ", activtyId=" + execution.getCurrentActivityId()
-                    + ", activtyName='" + execution.getCurrentActivityName() + "'"
-                    + ", processInstanceId=" + execution.getProcessInstanceId()
-                    + ", businessKey=" + execution.getProcessBusinessKey()
-                    + ", executionId=" + execution.getId()
-                    + " \n\n"
-                    + "STACKTRACE\n"
-                    + Arrays.toString(e.getStackTrace()));
+            CamundaLogger.log(execution, e, LoggerDelegate.class.getName());
         }
     }
 }
